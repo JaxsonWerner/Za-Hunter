f638e191d70dee7b6a8e83ced7f6b7972c1bf23d
Jaxson <Werner>
2026-04-02 10:21:57 -0500

    Display Location Info

---
+++ Za Hunter/LocationDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationDetailsView.swift
+//  Za Hunter
+//
+//  Created by Jaxson Werner on 4/2/26.
+//
+
+import SwiftUI
+
+struct LocationDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationDetailsView()
+}
